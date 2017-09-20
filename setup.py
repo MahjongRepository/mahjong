83f62bd5993ba253183f120567a2a42108c4b7b4
Nihisil <lisikhin@gmail.com>
2017-09-20 22:06:32 +0800

    Add missed packages tot he build script

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from distutils.core import setup
 description = """
 A python module for calculating riichi mahjong hands: yaku, han and fu.
 
+Right now it supports only japanese version (riichi mahjong). MCR (chinese version) in plans
+
 You can find usage examples here https://github.com/MahjongRepository/mahjong
 """
 
 setup(
     name='mahjong',
-    packages=['mahjong'],
-    version='1.0.1',
+    packages=[
+        'mahjong',
+        'mahjong.hand_calculating',
+        'mahjong.hand_calculating.yaku_list',
+        'mahjong.hand_calculating.yaku_list.yakuman',
+    ],
+    version='1.0.2',
     description='Mahjong hands calculation',
     long_description=description,
     author='Alexey Lisikhin',
