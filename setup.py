1c141dc0e0f0d17ec617b2c0510e0ef2924fbee3
Nihisil <alexey@nihisil.com>
2021-08-22 09:22:09 +0800

    Release 1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'mahjong.hand_calculating.yaku_list',
         'mahjong.hand_calculating.yaku_list.yakuman',
     ],
-    version='1.2.0.dev5',
+    version='1.2.0',
     description='Mahjong hands calculation',
     long_description=readme + '\n\n' + changelog,
     author='Alexey Lisikhin',
