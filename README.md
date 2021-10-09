f1784857de16674cbc6fd87d9991ca8bc0666ec1
Nihisil <alexey@nihisil.com>
2021-10-09 21:18:19 +0800

    Use new README file

---
+++ README.md
@@ -0,0 +1,17 @@
+This library can calculate hand cost (han, fu with details, yaku, and scores) for riichi mahjong (Japanese version).
+
+Also calculating of shanten is supported.
+
+The code was validated on tenhou.net phoenix replays in total on **11,120,125 hands**.
+
+So, we can say that our hand calculator works the same way that tenhou.net hand calculation.
+
+# How to install
+
+```bash
+pip install mahjong
+```
+
+# Supported rules and usage examples
+
+You can find usage examples and information about all supported rules variations in the [wiki](https://github.com/MahjongRepository/mahjong/wiki)
--- setup.py
+++ setup.py
@@ -1,39 +1,37 @@
 import setuptools
 
 from distutils.core import setup
+from pathlib import Path
 
-
-with open('README.rst', 'r') as f:
-    readme = f.read()
-
-with open('CHANGELOG.rst', 'r') as f:
-    changelog = f.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='mahjong',
+    name="mahjong",
     packages=[
-        'mahjong',
-        'mahjong.hand_calculating',
-        'mahjong.hand_calculating.yaku_list',
-        'mahjong.hand_calculating.yaku_list.yakuman',
+        "mahjong",
+        "mahjong.hand_calculating",
+        "mahjong.hand_calculating.yaku_list",
+        "mahjong.hand_calculating.yaku_list.yakuman",
     ],
-    version='1.2.0.dev6',
-    description='Mahjong hands calculation',
-    long_description=readme + '\n\n' + changelog,
-    author='Alexey Lisikhin',
-    author_email='alexey@nihisil.com',
-    url='https://github.com/MahjongRepository/mahjong',
-    data_files=[('', ['README.rst', 'CHANGELOG.rst'])],
+    version="1.2.0.dev6",
+    description="Mahjong hands calculation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alexey Lisikhin",
+    author_email="alexey@nihisil.com",
+    url="https://github.com/MahjongRepository/mahjong",
+    license='MIT',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
