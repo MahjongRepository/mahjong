460677c320536ac64c22693c8784892bbc1adf1a
Apricot-S <mahjong_apricot@outlook.jp>
2025-04-28 16:56:23 +0000

    refactor: Format with Ruff

--- doc/examples.py
+++ doc/examples.py
@@ -10,11 +10,11 @@ calculator = HandCalculator()
 # useful helper
 def print_hand_result(hand_result):
     print(hand_result.han, hand_result.fu)
-    print(hand_result.cost['main'])
+    print(hand_result.cost["main"])
     print(hand_result.yaku)
     for fu_item in hand_result.fu_details:
         print(fu_item)
-    print('')
+    print("")
 
 
 ####################################################################
@@ -23,8 +23,8 @@ def print_hand_result(hand_result):
 
 
 # we had to use all 14 tiles in that array
-tiles = TilesConverter.string_to_136_array(man='22444', pin='333567', sou='444')
-win_tile = TilesConverter.string_to_136_array(sou='4')[0]
+tiles = TilesConverter.string_to_136_array(man="22444", pin="333567", sou="444")
+win_tile = TilesConverter.string_to_136_array(sou="4")[0]
 
 result = calculator.estimate_hand_value(tiles, win_tile)
 print_hand_result(result)
@@ -44,9 +44,11 @@ print_hand_result(result)
 ####################################################################
 
 
-melds = [Meld(meld_type=Meld.PON, tiles=TilesConverter.string_to_136_array(man='444'))]
+melds = [Meld(meld_type=Meld.PON, tiles=TilesConverter.string_to_136_array(man="444"))]
 
-result = calculator.estimate_hand_value(tiles, win_tile, melds=melds, config=HandConfig(options=OptionalRules(has_open_tanyao=True)))
+result = calculator.estimate_hand_value(
+    tiles, win_tile, melds=melds, config=HandConfig(options=OptionalRules(has_open_tanyao=True))
+)
 print_hand_result(result)
 
 
@@ -56,7 +58,7 @@ print_hand_result(result)
 
 
 shanten = Shanten()
-tiles = TilesConverter.string_to_34_array(man='13569', pin='123459', sou='443')
+tiles = TilesConverter.string_to_34_array(man="13569", pin="123459", sou="443")
 result = shanten.calculate_shanten(tiles)
 
 print(result)
@@ -66,17 +68,15 @@ print(result)
 ####################################################################
 
 
-tiles = TilesConverter.string_to_136_array(man='222244466677788')
-win_tile = TilesConverter.string_to_136_array(man='7')[0]
-melds = [
-    Meld(Meld.KAN, TilesConverter.string_to_136_array(man='2222'), False)
-]
+tiles = TilesConverter.string_to_136_array(man="222244466677788")
+win_tile = TilesConverter.string_to_136_array(man="7")[0]
+melds = [Meld(Meld.KAN, TilesConverter.string_to_136_array(man="2222"), False)]
 
 dora_indicators = [
-    TilesConverter.string_to_136_array(man='1')[0],
-    TilesConverter.string_to_136_array(man='1')[0],
-    TilesConverter.string_to_136_array(man='1')[0],
-    TilesConverter.string_to_136_array(man='1')[0],
+    TilesConverter.string_to_136_array(man="1")[0],
+    TilesConverter.string_to_136_array(man="1")[0],
+    TilesConverter.string_to_136_array(man="1")[0],
+    TilesConverter.string_to_136_array(man="1")[0],
 ]
 
 config = HandConfig(is_riichi=True, options=OptionalRules(kazoe_limit=HandConfig.KAZOE_SANBAIMAN))
@@ -93,8 +93,8 @@ config = HandConfig(is_renhou=True)
 # renhou as an yakuman - old style
 config.yaku.renhou.han_closed = 13
 
-tiles = TilesConverter.string_to_136_array(man='22444', pin='333567', sou='444')
-win_tile = TilesConverter.string_to_136_array(sou='4')[0]
+tiles = TilesConverter.string_to_136_array(man="22444", pin="333567", sou="444")
+win_tile = TilesConverter.string_to_136_array(sou="4")[0]
 
 result = calculator.estimate_hand_value(tiles, win_tile, config=config)
 print_hand_result(result)
