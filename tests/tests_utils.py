76ed07634a666ad38e1aa07ec7ebf2b0be041c44
Apricot S. <152096077+Apricot-S@users.noreply.github.com>
2025-05-08 23:00:39 +0900

    chore: Move `tests` directories to align with standard project structures (#58)
    * test: Move the test files to `/tests/`
    
    * test: Fix module paths
    
    * chore: Modify format/lint target
    
    * chore: Change CI to use uv
    
    * chore: Match the Python version used in CI to `requires-python`

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,4 +1,4 @@
-from utils_for_tests import _string_to_34_tile
+from tests.utils_for_tests import _string_to_34_tile
 
 from mahjong.tile import TilesConverter
 from mahjong.utils import find_isolated_tile_indices, is_tile_strictly_isolated
