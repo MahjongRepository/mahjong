14d880fc2d941b0e40b6ab7b07d0f863af4930e5
Apricot S. <152096077+Apricot-S@users.noreply.github.com>
2025-10-11 22:53:56 +0900

    feat: Deprecate internal state properties of Shanten (#83)
    * feat: Deprecate internal state properties with warnings
    
    * feat: Add setters

--- mahjong/shanten.py
+++ mahjong/shanten.py
@@ -7,14 +7,104 @@ from mahjong.constants import HONOR_INDICES, TERMINAL_INDICES
 class Shanten:
     AGARI_STATE = -1
 
-    tiles: list[int] = []
-    number_melds = 0
-    number_tatsu = 0
-    number_pairs = 0
-    number_jidahai = 0
+    _tiles: list[int] = []
+    _number_melds = 0
+    _number_tatsu = 0
+    _number_pairs = 0
+    _number_jidahai = 0
     _flag_four_copies = 0
     _flag_isolated_tiles = 0
-    min_shanten = 0
+    _min_shanten = 0
+
+    @property
+    def tiles(self) -> list[int]:
+        warnings.warn(
+            "`tiles` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._tiles
+
+    @tiles.setter
+    def tiles(self, value: list[int]) -> None:
+        warnings.warn(
+            "`tiles` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._tiles = value
+
+    @property
+    def number_melds(self) -> int:
+        warnings.warn(
+            "`number_melds` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._number_melds
+
+    @number_melds.setter
+    def number_melds(self, value: int) -> None:
+        warnings.warn(
+            "`number_melds` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._number_melds = value
+
+    @property
+    def number_tatsu(self) -> int:
+        warnings.warn(
+            "`number_tatsu` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._number_tatsu
+
+    @number_tatsu.setter
+    def number_tatsu(self, value: int) -> None:
+        warnings.warn(
+            "`number_tatsu` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._number_tatsu = value
+
+    @property
+    def number_pairs(self) -> int:
+        warnings.warn(
+            "`number_pairs` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._number_pairs
+
+    @number_pairs.setter
+    def number_pairs(self, value: int) -> None:
+        warnings.warn(
+            "`number_pairs` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._number_pairs = value
+
+    @property
+    def number_jidahai(self) -> int:
+        warnings.warn(
+            "`number_jidahai` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._number_jidahai
+
+    @number_jidahai.setter
+    def number_jidahai(self, value: int) -> None:
+        warnings.warn(
+            "`number_jidahai` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._number_jidahai = value
 
     @property
     def number_characters(self) -> int:
@@ -25,6 +115,15 @@ class Shanten:
         )
         return self._flag_four_copies
 
+    @number_characters.setter
+    def number_characters(self, value: int) -> None:
+        warnings.warn(
+            "`number_characters` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._flag_four_copies = value
+
     @property
     def number_isolated_tiles(self) -> int:
         warnings.warn(
@@ -33,6 +132,33 @@ class Shanten:
             stacklevel=2,
         )
         return self._flag_isolated_tiles
+
+    @number_isolated_tiles.setter
+    def number_isolated_tiles(self, value: int) -> None:
+        warnings.warn(
+            "`number_isolated_tiles` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._flag_isolated_tiles = value
+
+    @property
+    def min_shanten(self) -> int:
+        warnings.warn(
+            "`min_shanten` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._min_shanten
+
+    @min_shanten.setter
+    def min_shanten(self, value: int) -> None:
+        warnings.warn(
+            "`min_shanten` is deprecated. This attribute reflects internal state and should not be used.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._min_shanten = value
 
     def calculate_shanten(self, tiles_34: Sequence[int], use_chiitoitsu: bool = True, use_kokushi: bool = True) -> int:
         """
@@ -90,29 +216,29 @@ class Shanten:
         init_mentsu = (14 - count_of_tiles) // 3
         self._scan(init_mentsu)
 
-        return self.min_shanten
+        return self._min_shanten
 
     def _init(self, tiles: list[int]) -> None:
-        self.tiles = tiles
-        self.number_melds = 0
-        self.number_tatsu = 0
-        self.number_pairs = 0
-        self.number_jidahai = 0
+        self._tiles = tiles
+        self._number_melds = 0
+        self._number_tatsu = 0
+        self._number_pairs = 0
+        self._number_jidahai = 0
         self._flag_four_copies = 0
         self._flag_isolated_tiles = 0
-        self.min_shanten = 8
+        self._min_shanten = 8
 
     def _scan(self, init_mentsu: int) -> None:
         for i in range(0, 27):
-            self._flag_four_copies |= (self.tiles[i] == 4) << i
-        self.number_melds += init_mentsu
+            self._flag_four_copies |= (self._tiles[i] == 4) << i
+        self._number_melds += init_mentsu
         self._run(0)
 
     def _run(self, depth: int) -> None:
-        if self.min_shanten == Shanten.AGARI_STATE:
+        if self._min_shanten == Shanten.AGARI_STATE:
             return
 
-        while not self.tiles[depth]:
+        while not self._tiles[depth]:
             depth += 1
 
             if depth >= 27:
@@ -127,10 +253,10 @@ class Shanten:
         if i > 8:
             i -= 9
 
-        if self.tiles[depth] == 4:
+        if self._tiles[depth] == 4:
             self._increase_set(depth)
-            if i < 7 and self.tiles[depth + 2]:
-                if self.tiles[depth + 1]:
+            if i < 7 and self._tiles[depth + 2]:
+                if self._tiles[depth + 1]:
                     self._increase_syuntsu(depth)
                     self._run(depth + 1)
                     self._decrease_syuntsu(depth)
@@ -138,7 +264,7 @@ class Shanten:
                 self._run(depth + 1)
                 self._decrease_tatsu_second(depth)
 
-            if i < 8 and self.tiles[depth + 1]:
+            if i < 8 and self._tiles[depth + 1]:
                 self._increase_tatsu_first(depth)
                 self._run(depth + 1)
                 self._decrease_tatsu_first(depth)
@@ -149,8 +275,8 @@ class Shanten:
             self._decrease_set(depth)
             self._increase_pair(depth)
 
-            if i < 7 and self.tiles[depth + 2]:
-                if self.tiles[depth + 1]:
+            if i < 7 and self._tiles[depth + 2]:
+                if self._tiles[depth + 1]:
                     self._increase_syuntsu(depth)
                     self._run(depth)
                     self._decrease_syuntsu(depth)
@@ -158,54 +284,54 @@ class Shanten:
                 self._run(depth + 1)
                 self._decrease_tatsu_second(depth)
 
-            if i < 8 and self.tiles[depth + 1]:
+            if i < 8 and self._tiles[depth + 1]:
                 self._increase_tatsu_first(depth)
                 self._run(depth + 1)
                 self._decrease_tatsu_first(depth)
 
             self._decrease_pair(depth)
 
-        if self.tiles[depth] == 3:
+        if self._tiles[depth] == 3:
             self._increase_set(depth)
             self._run(depth + 1)
             self._decrease_set(depth)
             self._increase_pair(depth)
 
-            if i < 7 and self.tiles[depth + 1] and self.tiles[depth + 2]:
+            if i < 7 and self._tiles[depth + 1] and self._tiles[depth + 2]:
                 self._increase_syuntsu(depth)
                 self._run(depth + 1)
                 self._decrease_syuntsu(depth)
             else:
-                if i < 7 and self.tiles[depth + 2]:
+                if i < 7 and self._tiles[depth + 2]:
                     self._increase_tatsu_second(depth)
                     self._run(depth + 1)
                     self._decrease_tatsu_second(depth)
 
-                if i < 8 and self.tiles[depth + 1]:
+                if i < 8 and self._tiles[depth + 1]:
                     self._increase_tatsu_first(depth)
                     self._run(depth + 1)
                     self._decrease_tatsu_first(depth)
 
             self._decrease_pair(depth)
 
-            if i < 7 and self.tiles[depth + 2] >= 2 and self.tiles[depth + 1] >= 2:
+            if i < 7 and self._tiles[depth + 2] >= 2 and self._tiles[depth + 1] >= 2:
                 self._increase_syuntsu(depth)
                 self._increase_syuntsu(depth)
                 self._run(depth)
                 self._decrease_syuntsu(depth)
                 self._decrease_syuntsu(depth)
 
-        if self.tiles[depth] == 2:
+        if self._tiles[depth] == 2:
             self._increase_pair(depth)
             self._run(depth + 1)
             self._decrease_pair(depth)
-            if i < 7 and self.tiles[depth + 2] and self.tiles[depth + 1]:
+            if i < 7 and self._tiles[depth + 2] and self._tiles[depth + 1]:
                 self._increase_syuntsu(depth)
                 self._run(depth)
                 self._decrease_syuntsu(depth)
 
-        if self.tiles[depth] == 1:
-            if i < 6 and self.tiles[depth + 1] == 1 and self.tiles[depth + 2] and self.tiles[depth + 3] != 4:
+        if self._tiles[depth] == 1:
+            if i < 6 and self._tiles[depth + 1] == 1 and self._tiles[depth + 2] and self._tiles[depth + 3] != 4:
                 self._increase_syuntsu(depth)
                 self._run(depth + 2)
                 self._decrease_syuntsu(depth)
@@ -214,8 +340,8 @@ class Shanten:
                 self._run(depth + 1)
                 self._decrease_isolated_tile(depth)
 
-                if i < 7 and self.tiles[depth + 2]:
-                    if self.tiles[depth + 1]:
+                if i < 7 and self._tiles[depth + 2]:
+                    if self._tiles[depth + 1]:
                         self._increase_syuntsu(depth)
                         self._run(depth + 1)
                         self._decrease_syuntsu(depth)
@@ -223,16 +349,16 @@ class Shanten:
                     self._run(depth + 1)
                     self._decrease_tatsu_second(depth)
 
-                if i < 8 and self.tiles[depth + 1]:
+                if i < 8 and self._tiles[depth + 1]:
                     self._increase_tatsu_first(depth)
                     self._run(depth + 1)
                     self._decrease_tatsu_first(depth)
 
     def _update_result(self) -> None:
-        ret_shanten = 8 - self.number_melds * 2 - self.number_tatsu - self.number_pairs
-        n_mentsu_kouho = self.number_melds + self.number_tatsu
-        if self.number_pairs:
-            n_mentsu_kouho += self.number_pairs - 1
+        ret_shanten = 8 - self._number_melds * 2 - self._number_tatsu - self._number_pairs
+        n_mentsu_kouho = self._number_melds + self._number_tatsu
+        if self._number_pairs:
+            n_mentsu_kouho += self._number_pairs - 1
         elif self._flag_four_copies and self._flag_isolated_tiles:
             if (self._flag_four_copies | self._flag_isolated_tiles) == self._flag_four_copies:
                 ret_shanten += 1
@@ -240,66 +366,66 @@ class Shanten:
         if n_mentsu_kouho > 4:
             ret_shanten += n_mentsu_kouho - 4
 
-        if ret_shanten != Shanten.AGARI_STATE and ret_shanten < self.number_jidahai:
-            ret_shanten = self.number_jidahai
+        if ret_shanten != Shanten.AGARI_STATE and ret_shanten < self._number_jidahai:
+            ret_shanten = self._number_jidahai
 
-        if ret_shanten < self.min_shanten:
-            self.min_shanten = ret_shanten
+        if ret_shanten < self._min_shanten:
+            self._min_shanten = ret_shanten
 
     def _increase_set(self, k: int) -> None:
-        self.tiles[k] -= 3
-        self.number_melds += 1
+        self._tiles[k] -= 3
+        self._number_melds += 1
 
     def _decrease_set(self, k: int) -> None:
-        self.tiles[k] += 3
-        self.number_melds -= 1
+        self._tiles[k] += 3
+        self._number_melds -= 1
 
     def _increase_pair(self, k: int) -> None:
-        self.tiles[k] -= 2
-        self.number_pairs += 1
+        self._tiles[k] -= 2
+        self._number_pairs += 1
 
     def _decrease_pair(self, k: int) -> None:
-        self.tiles[k] += 2
-        self.number_pairs -= 1
+        self._tiles[k] += 2
+        self._number_pairs -= 1
 
     def _increase_syuntsu(self, k: int) -> None:
-        self.tiles[k] -= 1
-        self.tiles[k + 1] -= 1
-        self.tiles[k + 2] -= 1
-        self.number_melds += 1
+        self._tiles[k] -= 1
+        self._tiles[k + 1] -= 1
+        self._tiles[k + 2] -= 1
+        self._number_melds += 1
 
     def _decrease_syuntsu(self, k: int) -> None:
-        self.tiles[k] += 1
-        self.tiles[k + 1] += 1
-        self.tiles[k + 2] += 1
-        self.number_melds -= 1
+        self._tiles[k] += 1
+        self._tiles[k + 1] += 1
+        self._tiles[k + 2] += 1
+        self._number_melds -= 1
 
     def _increase_tatsu_first(self, k: int) -> None:
-        self.tiles[k] -= 1
-        self.tiles[k + 1] -= 1
-        self.number_tatsu += 1
+        self._tiles[k] -= 1
+        self._tiles[k + 1] -= 1
+        self._number_tatsu += 1
 
     def _decrease_tatsu_first(self, k: int) -> None:
-        self.tiles[k] += 1
-        self.tiles[k + 1] += 1
-        self.number_tatsu -= 1
+        self._tiles[k] += 1
+        self._tiles[k + 1] += 1
+        self._number_tatsu -= 1
 
     def _increase_tatsu_second(self, k: int) -> None:
-        self.tiles[k] -= 1
-        self.tiles[k + 2] -= 1
-        self.number_tatsu += 1
+        self._tiles[k] -= 1
+        self._tiles[k + 2] -= 1
+        self._number_tatsu += 1
 
     def _decrease_tatsu_second(self, k: int) -> None:
-        self.tiles[k] += 1
-        self.tiles[k + 2] += 1
-        self.number_tatsu -= 1
+        self._tiles[k] += 1
+        self._tiles[k + 2] += 1
+        self._number_tatsu -= 1
 
     def _increase_isolated_tile(self, k: int) -> None:
-        self.tiles[k] -= 1
+        self._tiles[k] -= 1
         self._flag_isolated_tiles |= 1 << k
 
     def _decrease_isolated_tile(self, k: int) -> None:
-        self.tiles[k] += 1
+        self._tiles[k] += 1
         self._flag_isolated_tiles &= ~(1 << k)
 
     def _remove_character_tiles(self, nc: int) -> None:
@@ -307,23 +433,23 @@ class Shanten:
         isolated = 0
 
         for i in range(27, 34):
-            if self.tiles[i] == 4:
-                self.number_melds += 1
-                self.number_jidahai += 1
+            if self._tiles[i] == 4:
+                self._number_melds += 1
+                self._number_jidahai += 1
                 four_copies |= 1 << (i - 27)
                 isolated |= 1 << (i - 27)
 
-            if self.tiles[i] == 3:
-                self.number_melds += 1
+            if self._tiles[i] == 3:
+                self._number_melds += 1
 
-            if self.tiles[i] == 2:
-                self.number_pairs += 1
+            if self._tiles[i] == 2:
+                self._number_pairs += 1
 
-            if self.tiles[i] == 1:
+            if self._tiles[i] == 1:
                 isolated |= 1 << (i - 27)
 
-        if self.number_jidahai and (nc % 3) == 2:
-            self.number_jidahai -= 1
+        if self._number_jidahai and (nc % 3) == 2:
+            self._number_jidahai -= 1
 
         if isolated:
             self._flag_isolated_tiles |= 1 << 27
