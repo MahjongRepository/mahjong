f600377bc8c24cedd839dcc370e7c35da90af04e
Apricot-S <mahjong_apricot@outlook.jp>
2026-01-27 17:26:19 +0900

    test: Rename result to expected

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -11,7 +11,7 @@ from tests.utils_for_tests import _string_to_34_tile
 
 
 @pytest.mark.parametrize(
-    ("sou", "pin", "man", "honors", "result"),
+    ("sou", "pin", "man", "honors", "expected"),
     [
         ("1", "", "", "", False),
         ("2", "", "", "", False),
@@ -49,16 +49,16 @@ from tests.utils_for_tests import _string_to_34_tile
         ("", "", "", "7", True),
     ],
 )
-def test_find_isolated_tiles(sou: str, pin: str, man: str, honors: str, result: bool) -> None:
+def test_find_isolated_tiles(sou: str, pin: str, man: str, honors: str, expected: bool) -> None:
     hand_34 = TilesConverter.string_to_34_array(sou="1369", pin="15678", man="25", honors="124")
     isolated_tiles = find_isolated_tile_indices(hand_34)
 
     tile_34 = _string_to_34_tile(sou=sou, pin=pin, man=man, honors=honors)
-    assert (tile_34 in isolated_tiles) == result
+    assert (tile_34 in isolated_tiles) == expected
 
 
 @pytest.mark.parametrize(
-    ("sou", "pin", "man", "honors", "result"),
+    ("sou", "pin", "man", "honors", "expected"),
     [
         ("1", "", "", "", False),
         ("2", "", "", "", False),
@@ -96,14 +96,14 @@ def test_find_isolated_tiles(sou: str, pin: str, man: str, honors: str, result: 
         ("", "", "", "7", True),
     ],
 )
-def test_is_strictly_isolated_tile(sou: str, pin: str, man: str, honors: str, result: bool) -> None:
+def test_is_strictly_isolated_tile(sou: str, pin: str, man: str, honors: str, expected: bool) -> None:
     hand_34 = TilesConverter.string_to_34_array(sou="1399", pin="1567", man="25", honors="1224")
     tile_34 = _string_to_34_tile(sou=sou, pin=pin, man=man, honors=honors)
-    assert is_tile_strictly_isolated(hand_34, tile_34) == result
+    assert is_tile_strictly_isolated(hand_34, tile_34) == expected
 
 
 @pytest.mark.parametrize(
-    ("sou", "pin", "man", "honors", "result"),
+    ("sou", "pin", "man", "honors", "expected"),
     [
         ("1", "", "", "", False),
         ("2", "", "", "", False),
@@ -141,9 +141,9 @@ def test_is_strictly_isolated_tile(sou: str, pin: str, man: str, honors: str, re
         ("", "", "", "7", False),
     ],
 )
-def test_is_dora_indicator_for_terminal(sou: str, pin: str, man: str, honors: str, result: bool) -> None:
+def test_is_dora_indicator_for_terminal(sou: str, pin: str, man: str, honors: str, expected: bool) -> None:
     tile_34 = _string_to_34_tile(sou=sou, pin=pin, man=man, honors=honors)
-    assert is_dora_indicator_for_terminal(tile_34) == result
+    assert is_dora_indicator_for_terminal(tile_34) == expected
 
 
 @pytest.mark.parametrize(
