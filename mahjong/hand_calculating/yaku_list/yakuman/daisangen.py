7b59096f7ee6b5536ca4fbfacba0cbf4e00e0537
Apricot-S <mahjong_apricot@outlook.jp>
2026-01-31 19:42:10 +0900

    refactor: Replace hardcoded dragon constants with DRAGONS in Daisangen

--- mahjong/hand_calculating/yaku_list/yakuman/daisangen.py
+++ mahjong/hand_calculating/yaku_list/yakuman/daisangen.py
@@ -1,6 +1,6 @@
 from collections.abc import Collection, Sequence
 
-from mahjong.constants import CHUN, HAKU, HATSU
+from mahjong.constants import DRAGONS
 from mahjong.hand_calculating.yaku import Yaku
 from mahjong.utils import is_pon_or_kan
 
@@ -26,6 +26,6 @@ class Daisangen(Yaku):
     def is_condition_met(self, hand: Collection[Sequence[int]], *args) -> bool:
         count_of_dragon_pon_sets = 0
         for item in hand:
-            if is_pon_or_kan(item) and item[0] in [CHUN, HAKU, HATSU]:
+            if is_pon_or_kan(item) and item[0] in DRAGONS:
                 count_of_dragon_pon_sets += 1
         return count_of_dragon_pon_sets == 3
