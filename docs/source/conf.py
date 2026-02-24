3706c02c0fe82f35cb0c5e0f147e9d9909f70b3e
Apricot-S <mahjong_apricot@outlook.jp>
2026-02-24 21:43:09 +0900

    docs: Update settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'mahjong'
-copyright = '2017, mahjong Python library contributors'
-author = 'mahjong Python library contributors'
-release = '2017'
+project = "mahjong"
+copyright = "2017, mahjong Python library contributors"
+author = "mahjong Python library contributors"
+release = "2017"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = []
+import sys
+from pathlib import Path
 
+sys.path.insert(0, str(Path("..", "docs").resolve()))
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,15 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
+:github_url: https://github.com/MahjongRepository/mahjong
+
 mahjong documentation
 =====================
-
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+.. automodule:: mahjong
+   :members:
